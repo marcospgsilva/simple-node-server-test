03cf6b6daaa287f65d100e03db32bf681c44fafa
Marcos Paulo <marcos@meucredere.com.br>
2022-10-14 09:53:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+    console.log(`server started on port ${PORT}`);
+});
+
+app.post('/webhooks', (req, res) => {
+    console.log('Webhook received!')
+    console.log(req)
+    res.send('POST request to the homepage')
+})
